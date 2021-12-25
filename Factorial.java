b4eda7bdbd0249a579a0395807ea85ff92c1bb3b
LahotyR <96655679+LahotyR@users.noreply.github.com>
2021-12-25 19:24:03 +0530

    Add files via upload
    Assignments for wee1-day1

---
+++ Factorial.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		
+		int i;
+		int fact = 1;
+		
+		for (i = 5; i >0 ; i--) {
+			
+			fact = i*fact;
+			
+	//		System.out.println("i " + i);
+	//		System.out.println("fact " + fact);		
+		}
+		
+		System.out.println("Factorial is: " + fact);
+		
+	}
+
+}
